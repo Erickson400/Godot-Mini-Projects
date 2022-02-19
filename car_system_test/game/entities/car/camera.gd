dbef31ee0e83aa4353d0c80dc494ec96098a55f6
Erickson Munoz <claudespeed400@gmail.com>
2022-02-19 08:20:16 -0500

    feb

--- car_system_test/game/entities/car/camera.gd
+++ car_system_test/game/entities/car/camera.gd
@@ -1,9 +1,12 @@
 extends Spatial
 
+onready var body = get_parent().get_node("Body")
+
 func _process(_delta):
-	var body = get_parent().get_node("Body")
-	var body_pos = body.global_transform.origin
-	global_transform.origin = body_pos
+	# Move the pivot to the car
+	global_transform.origin = body.global_transform.origin
+	
+	# Rotate it on the car's global Y axis
 	rotation.y = body.rotation.y
 
 
--- car_system_test/game/entities/car/car.gd
+++ car_system_test/game/entities/car/car.gd
@@ -1,39 +1,52 @@
 extends Spatial
 
-
-
+# Steering Constants
 const STEER_FORCE = 1
+const COUNTER_STEER_FORCE = 5
 const MAX_STEER_ANGLE = deg2rad(30)
+
+# Engine Constants
+var ENGINE_POWER = 60
+
+
 
 func _physics_process(_delta):
 	var steer_strength = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	steer(steer_strength)
+	
+	var gas = 0
+	var sensitivity = 1000
+	if Input.is_action_pressed("ui_up"):
+		gas = sensitivity
+	elif Input.is_action_pressed("ui_down"):
+		gas = -sensitivity
+	else:
+		gas = 0
+		
+	var current_velocity = $BRJoint.get_param_y(Generic6DOFJoint.PARAM_ANGULAR_MOTOR_TARGET_VELOCITY)
+	var final = gas
+	
+	$BRJoint.set_param_x(Generic6DOFJoint.PARAM_ANGULAR_MOTOR_FORCE_LIMIT, final)
+	$BLJoint.set_param_x(Generic6DOFJoint.PARAM_ANGULAR_MOTOR_FORCE_LIMIT, final)
+
+
+
+func accelerate():
+	pass
+
 
 func steer(amount):
 	if amount < 0.1 and  amount > -0.1:
-		# Don't steer if amount is close to zero
-		$FLJoint.set_param_y(Generic6DOFJoint.PARAM_ANGULAR_LOWER_LIMIT, 0)
-		$FLJoint.set_param_y(Generic6DOFJoint.PARAM_ANGULAR_UPPER_LIMIT, 0)
-		$FRJoint.set_param_y(Generic6DOFJoint.PARAM_ANGULAR_LOWER_LIMIT, 0)
-		$FRJoint.set_param_y(Generic6DOFJoint.PARAM_ANGULAR_UPPER_LIMIT, 0)
-		
+		# Counter steer if amount is close to zero
+		var basis1 = -$Body.global_transform.basis.x
+		var basis2 = -$FLWheel.global_transform.basis.x
+		var target_vel = basis1.cross(basis2).y * COUNTER_STEER_FORCE
+		$FLJoint.set_param_y(Generic6DOFJoint.PARAM_ANGULAR_MOTOR_TARGET_VELOCITY, target_vel)
+		$FRJoint.set_param_y(Generic6DOFJoint.PARAM_ANGULAR_MOTOR_TARGET_VELOCITY, target_vel)
 	else:
-		# Steer
-		if amount > 0:
-			# Increase Upper limit so it can move
-			$FLJoint.set_param_y(Generic6DOFJoint.PARAM_ANGULAR_UPPER_LIMIT, MAX_STEER_ANGLE * amount)
-			$FRJoint.set_param_y(Generic6DOFJoint.PARAM_ANGULAR_UPPER_LIMIT, MAX_STEER_ANGLE * amount)
-		else:
-			# Increase Lower limit so it can move
-			$FLJoint.set_param_y(Generic6DOFJoint.PARAM_ANGULAR_LOWER_LIMIT, MAX_STEER_ANGLE * amount)
-			$FRJoint.set_param_y(Generic6DOFJoint.PARAM_ANGULAR_LOWER_LIMIT, MAX_STEER_ANGLE * amount)
-		
-		# Move the wheels
+		# Steer by moving the wheels
 		$FLJoint.set_param_y(Generic6DOFJoint.PARAM_ANGULAR_MOTOR_TARGET_VELOCITY, amount * STEER_FORCE)
 		$FRJoint.set_param_y(Generic6DOFJoint.PARAM_ANGULAR_MOTOR_TARGET_VELOCITY, amount * STEER_FORCE)
-
-
-
 
 
 
