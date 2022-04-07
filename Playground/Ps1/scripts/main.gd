b5bb2da290fe67113d1134941ef01ba35f03b618
Erickson Munoz <claudespeed400@gmail.com>
2022-04-07 13:14:52 -0400

    initial

--- Playground/Ps1/scripts/main.gd
+++ Playground/Ps1/scripts/main.gd
@@ -6,9 +6,9 @@ var my_res = null
 
 
 
-func _ready():
-	my_res = load(res_path)
-	assert(my_res == null, "Could not load resource")
+#func _ready():
+#my_res = load(res_path)
+	#assert(my_res == null, "Could not load resource")
 	
 
 
